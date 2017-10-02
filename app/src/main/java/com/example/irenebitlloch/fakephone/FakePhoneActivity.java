96fac1adf4dd96f0ef92900a5592b7be5a7c552b
irene.bitlloch <irene.bitlloch@gmail.com>
2017-10-02 15:51:45 +0200

    Versió inicial

---
+++ app/src/main/java/com/example/irenebitlloch/fakephone/FakePhoneActivity.java
@@ -0,0 +1,22 @@
+package com.example.irenebitlloch.fakephone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class FakePhoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fake_phone);
+    }
+
+    public void clicat (View v){
+        Button boto = (Button) v;
+        Log.i("click", "Has clicat el botó!");
+
+    }
+}
